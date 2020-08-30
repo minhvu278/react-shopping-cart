5a814bfa7f4e80a510ced22d371ed9f88f6f0146
vu <dominhvu278@gmail.com>
2020-08-30 17:53:24 +0700

    connect mongo

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(bodyParser.json());
 
 //deploy
-mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
-    userNewUrlParse: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
+//     userNewUrlParse: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// });
 
 // Mongodb
 const Product = mongoose.model(
@@ -28,7 +28,81 @@ const Product = mongoose.model(
 )
 
 app.get("/api/products", async (req, res) => {
-    const products = await Product.find({});
+    // const products = await Product.find({});
+    const products = [
+        {
+            "_id": "8JlCq1yvw",
+            "__v": 0,
+            "availableSizes": ["X", "L", "XL", "XXL"],
+            "description": "Mẫu số 1",
+            "image": "/images/dress1.jpg",
+            "price": 29.9,
+            "title": "Dress 1"
+        },
+        {
+            "_id": "I2ivd9tKl",
+            "__v": 0,
+            "availableSizes": ["X", "L", "XL", "M", "XXL"],
+            "description": "Mẫu số 2",
+            "image": "/images/dress2.jpg",
+            "price": 18.9,
+            "title": "Dress 2"
+        },
+        {
+            "_id": "MLc5ipULj",
+            "__v": 0,
+            "availableSizes": ["X", "L", "XL", "XXL"],
+            "description": "Mẫu số 3",
+            "image": "/images/dress3.jpg",
+            "price": 19.9,
+            "title": "Dress 3"
+        },
+        {
+            "_id": "KMFv5RPVn",
+            "__v": 0,
+            "availableSizes": ["X", "L", "XL", "XXL"],
+            "description": "Mẫu số 4",
+            "image": "/images/dress4.jpg",
+            "price": 39.9,
+            "title": "Dress 4"
+        },
+        {
+            "_id": "tYAAB-ti_",
+            "__v": 0,
+            "availableSizes": ["X", "L", "XL", "S", "XXL"],
+            "description": "Mẫu số 5",
+            "image": "/images/dress5.jpg",
+            "price": 49.9,
+            "title": "Dress 5"
+        },
+        {
+            "_id": "6aMbtpfDc",
+            "__v": 0,
+            "availableSizes": ["X", "L", "XL", "XXL"],
+            "description": "Mẫu số 6",
+            "image": "/images/dress6.jpg",
+            "price": 29.9,
+            "title": "Dress 6"
+        },
+        {
+            "_id": "ZcoGSX8HU",
+            "__v": 0,
+            "availableSizes": ["X", "L", "XL", "XXL"],
+            "description": "Mẫu số 7",
+            "image": "/images/dress3.jpg",
+            "price": 27.9,
+            "title": "Dress 7"
+        },
+        {
+            "_id": "FA9rpHGoJ",
+            "__v": 0,
+            "availableSizes": ["X", "L", "XL", "XXL"],
+            "description": "Mẫu số 7",
+            "image": "/images/dress3.jpg",
+            "price": 27.9,
+            "title": "Dress 7"
+        }
+    ]
     res.send(products)
 });
 
